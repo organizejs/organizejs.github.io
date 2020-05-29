bd3f309b285bf4a6b4b7ef496b385803cd1e4adb
organizejs <organizejs@gmail.com>
2020-05-29 04:17:23 +0000

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,35 @@
-// entry point
-$(document).ready(function () {})
+var data_url = "https://raw.githubusercontent.com/organizejs/organizejs.github.io/master/data.json?token=AGDVVD72DOCIN6MMRODIXYK63G2X2"; 
+
+function getTemplate(date, link, title, publication, description) {
+  return `
+  <tr>
+    <td class="date">${date}</td>
+    <td class="link"><a href="${link}" target="_blank">${title}</a></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td class="publication">${publication}</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td class="description">${description}</td>
+  </tr>
+  `
+}
+
+$(document).ready(function() {
+  $.getJSON(data_url, function(data) {
+    for (var i in data) {
+      let e = data[i]
+      let template = getTemplate(
+        e["date"],
+        e["link"],
+        e["title"],
+        e["publication"],
+        e["description"]
+      );
+      $("#writing").append($(template));
+    }
+  });
+
+})
